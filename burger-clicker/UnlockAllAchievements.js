9d4b3a853fdf11c39b108adf62125aed35f533bc
sumde2 <jason.radatus@gmail.com>
2025-01-17 20:47:20 -0500

    Create UnlockAllAchievements.js

---
+++ burger-clicker/UnlockAllAchievements.js
@@ -0,0 +1,2 @@
+// You will need to wait at least 5 seconds before all the achievements become unlocked.
+achievement.awarded = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true];
